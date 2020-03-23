8184c6c39fb185aa40f8ed6dae505542793b159b
Sampatij2023 <58723466+Sampatij2023@users.noreply.github.com>
2020-03-24 00:26:42 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,106 @@
+// alert("Hello guys");
+// for (var i = 0 ; i< document.querySelectorAll(".drum").length; i++){
+// document.querySelectorAll("button")[i].addEventListener("click", handleClick);
+// function handleClick(){
+//   alert("I got clicked");
+// }}
+for (var i=0; i<document.querySelectorAll(".drum").length; i++){
+  document.querySelectorAll("button")[i].addEventListener("click", function (){
+    // alert("I got clicked");
+    // console.log(this.innerHTML);
+    // var audio = new Audio('sounds/tom-1.mp3');
+    // audio.play();
+    // this.style.color = "green";
+
+
+
+    var buttonInnerHTML= this.innerHTML;
+    makeSound(nuttonInnerHTML);});}
+    // switch (buttonInnerHTML) {
+    //   case "w":
+    //   var tom1 = new Audio('sounds/tom-1.mp3');
+    //    tom1.play();
+    //      break;
+    //
+    //    case "a":
+    //    var tom2 = new Audio('sounds/tom-2.mp3');
+    //     tom2.play();
+    //       break;
+    //
+    //     case "s":
+    //     var tom3 = new Audio('sounds/tom-3.mp3');
+    //      tom3.play();
+    //        break;
+    //
+    //      case "d":
+    //      var tom4 = new Audio('sounds/tom-4.mp3');
+    //       tom4.play();
+    //         break;
+    //
+    //       case "l":
+    //       var crash = new Audio('sounds/crash.mp3');
+    //        crash.play();
+    //          break;
+    //
+    //        case "k":
+    //        var kick = new Audio('sounds/kick-bass.mp3');
+    //         kick.play();
+    //           break;
+    //
+    //         case "j":
+    //         var snare = new Audio('sounds/snare.mp3');
+    //          snare.play();
+    //            break;
+    //
+    //       default:console.log(this.innerHTML);
+
+
+
+          // function Audio (fileLocation){
+          //   this.fileLocation = fileLocation;
+          //   this.play = function() {
+          //
+          //   }
+          // }
+          document.addEventListener("keypress", function(event){
+            makeSound(event.key);
+          });
+          function makeSound(key){
+            switch (key) {
+              case "w":
+              var tom1 = new Audio('sounds/tom-1.mp3');
+               tom1.play();
+                 break;
+
+               case "a":
+               var tom2 = new Audio('sounds/tom-2.mp3');
+                tom2.play();
+                  break;
+
+                case "s":
+                var tom3 = new Audio('sounds/tom-3.mp3');
+                 tom3.play();
+                   break;
+
+                 case "d":
+                 var tom4 = new Audio('sounds/tom-4.mp3');
+                  tom4.play();
+                    break;
+
+                  case "l":
+                  var crash = new Audio('sounds/crash.mp3');
+                   crash.play();
+                     break;
+
+                   case "k":
+                   var kick = new Audio('sounds/kick-bass.mp3');
+                    kick.play();
+                      break;
+
+                    case "j":
+                    var snare = new Audio('sounds/snare.mp3');
+                     snare.play();
+                       break;
+
+                  default:console.log(this.innerHTML);
+          }}
