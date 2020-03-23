c462d228f86a57ac4238d90b7e10cb21515b3014
Sampatij2023 <58723466+Sampatij2023@users.noreply.github.com>
2020-03-24 02:59:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,9 @@ for (var i=0; i<document.querySelectorAll(".drum").length; i++){
 
 
     var buttonInnerHTML= this.innerHTML;
-    makeSound(nuttonInnerHTML);});}
+    makeSound(buttonInnerHTML);
+    buttonAnimation(buttonInnerHTML);
+  });}
     // switch (buttonInnerHTML) {
     //   case "w":
     //   var tom1 = new Audio('sounds/tom-1.mp3');
@@ -64,6 +66,7 @@ for (var i=0; i<document.querySelectorAll(".drum").length; i++){
           // }
           document.addEventListener("keypress", function(event){
             makeSound(event.key);
+            buttonAnimation(event.key);
           });
           function makeSound(key){
             switch (key) {
@@ -104,3 +107,9 @@ for (var i=0; i<document.querySelectorAll(".drum").length; i++){
 
                   default:console.log(this.innerHTML);
           }}
+function buttonAnimation(currentKey){
+var activeButton = document.querySelector("."+currentKey);
+  activeButton.classList.add("pressed");
+  setTimeout(function(){
+    activeButton.classList.remove("pressed");},100););
+}
